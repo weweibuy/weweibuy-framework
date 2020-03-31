eb3a0fcbcd4f2e5d830581523d1915a1aa8bbd0a
renhao.du <ak514250@126.com>
2020-03-31 16:15:26 +0800

    删除无用注释

--- idempotent/idempotent-core/src/main/java/com/weweibuy/framework/idempotent/core/aspect/IdempotentBeanFactoryPointcutAdvisor.java
+++ idempotent/idempotent-core/src/main/java/com/weweibuy/framework/idempotent/core/aspect/IdempotentBeanFactoryPointcutAdvisor.java
@@ -1,8 +1,3 @@
-/*
- * All rights Reserved, Designed By baowei
- *
- * 注意：本内容仅限于内部传阅，禁止外泄以及用于其他的商业目的
- */
 package com.weweibuy.framework.idempotent.core.aspect;
 
 import org.springframework.aop.Pointcut;
