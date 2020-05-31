b55af47a47e793d9ca559e4de7067d8269b17446
杜仁浩 <ak514250@126.com>
2020-05-31 13:12:37 +0800

    更改 部分工具

--- common/common-core/src/main/java/com.weiweibuy.framework.common.core/utils/DateTimeUtils.java
+++ common/common-core/src/main/java/com.weiweibuy.framework.common.core/utils/DateTimeUtils.java
@@ -5,10 +5,13 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * 时间日期工具
+ *
  * @author durenhao
  * @date 2019/7/29 18:14
  **/
@@ -16,33 +19,95 @@ import java.util.concurrent.TimeUnit;
 public class DateTimeUtils {
 
 
+    /**
+     * 转为标准时间日期 String
+     *
+     * @param date
+     * @return
+     */
     public static String toStringDate(Date date) {
         LocalDateTime localDateTime = dateToLocalDateTime(date);
         return localDateTime.format(CommonConstant.DateConstant.STANDARD_DATE_TIME_FORMATTER);
     }
 
 
-    public static String toDateFormat(LocalDateTime date) {
+    /**
+     * 转为标准时间日期 String
+     *
+     * @param date
+     * @return
+     */
+    public static String toStringDate(LocalDateTime date) {
         return date.format(CommonConstant.DateConstant.STANDARD_DATE_TIME_FORMATTER);
     }
 
+    /**
+     * 转为时间日期 String
+     *
+     * @param date
+     * @param formatter
+     * @return
+     */
+    public static String toStringDate(LocalDateTime date, DateTimeFormatter formatter) {
+        return date.format(formatter);
+    }
+
+    /**
+     * date 转 LocalDateTime
+     *
+     * @param date
+     * @return
+     */
     public static LocalDateTime dateToLocalDateTime(Date date) {
         return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 
-    public static LocalDateTime systemTimestampToLocalDateTime(long timestamp) {
-        return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
-    }
-
+    /**
+     * LocalDateTime 转 date
+     *
+     * @param localDateTime
+     * @return
+     */
     public static Date localDateTimeToDate(LocalDateTime localDateTime) {
         return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 
+    /**
+     * 毫秒时间戳转 LocalDateTime
+     *
+     * @param timestampMilli
+     * @return
+     */
+    public static LocalDateTime timestampMilliToLocalDateTime(long timestampMilli) {
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestampMilli), ZoneId.systemDefault());
+    }
 
+    /**
+     * 秒时间转 LocalDateTime
+     *
+     * @param timestampSecond
+     * @return
+     */
+    public static LocalDateTime timestampSecondToLocalDateTime(long timestampSecond) {
+        return LocalDateTime.ofInstant(Instant.ofEpochSecond(timestampSecond), ZoneId.systemDefault());
+    }
+
+    /**
+     * LocalDateTime 转毫秒时间
+     *
+     * @param localDateTime
+     * @return
+     */
     public static long localDateTimeToTimestampMilli(LocalDateTime localDateTime) {
         return localDateTime.toInstant(ZoneOffset.of(CommonConstant.DateConstant.TIME_OFFSET_ID)).toEpochMilli();
     }
 
+    /**
+     * LocalDateTime 转秒时间
+     *
+     * @param localDateTime
+     * @return
+     */
     public static long localDateTimeToTimestampSecond(LocalDateTime localDateTime) {
         return localDateTime.toEpochSecond(ZoneOffset.of(CommonConstant.DateConstant.TIME_OFFSET_ID));
     }
@@ -52,12 +117,37 @@ public class DateTimeUtils {
         return timeUnit.toMillis(duration);
     }
 
+    /**
+     * 当前时间 是否晚于 给定时间 localDateTime   interval 值
+     *
+     * @param localDateTime
+     * @param interval
+     * @return
+     */
     public static boolean isCurrentTimeOverInterval(LocalDateTime localDateTime, long interval) {
         return System.currentTimeMillis() - localDateTimeToTimestampMilli(localDateTime) > interval;
     }
 
+    /**
+     * 时间差
+     *
+     * @param start
+     * @param end
+     * @return
+     */
     public static Duration between(LocalDateTime start, LocalDateTime end) {
         return Duration.between(start, end);
+    }
+
+    /**
+     * 时间差毫秒值
+     *
+     * @param start
+     * @param end
+     * @return
+     */
+    public static long betweenMilli(LocalDateTime start, LocalDateTime end) {
+        return Duration.between(start, end).toMillis();
     }
 
 
