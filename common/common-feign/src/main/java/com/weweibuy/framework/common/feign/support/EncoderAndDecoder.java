fd9a9e21fb31f45272cc5d45a80f0d72fe121447
杜仁浩 <ak514250@126.com>
2020-06-16 22:42:26 +0800

    简化 feign decoder 代码

--- common/common-feign/src/main/java/com/weweibuy/framework/common/feign/support/EncoderAndDecoder.java
+++ common/common-feign/src/main/java/com/weweibuy/framework/common/feign/support/EncoderAndDecoder.java
@@ -1,8 +1,8 @@
 package com.weweibuy.framework.common.feign.support;
 
 import feign.Response;
-import feign.Util;
 import feign.codec.Decoder;
+import feign.optionals.OptionalDecoder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
@@ -11,9 +11,7 @@ import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 
 import java.io.IOException;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Optional;
 
 /**
  * @author durenhao
@@ -26,29 +24,13 @@ public class EncoderAndDecoder extends SpringEncoder implements Decoder {
 
     public EncoderAndDecoder(ObjectFactory<HttpMessageConverters> messageConverters) {
         super(messageConverters);
-        this.delegate = new ResponseEntityDecoder(new SpringDecoder(messageConverters));
+        this.delegate = new OptionalDecoder(new ResponseEntityDecoder(new SpringDecoder(messageConverters)));
     }
 
 
     @Override
     public Object decode(Response response, Type type) throws IOException {
-        if (!isOptional(type)) {
-            return delegate.decode(response, type);
-        }
-        if (response.status() == 404 || response.status() == 204) {
-            return Optional.empty();
-        }
-        Type enclosedType = Util.resolveLastTypeParameter(type, Optional.class);
-        return Optional.ofNullable(delegate.decode(response, enclosedType));
-    }
-
-
-    static boolean isOptional(Type type) {
-        if (!(type instanceof ParameterizedType)) {
-            return false;
-        }
-        ParameterizedType parameterizedType = (ParameterizedType) type;
-        return parameterizedType.getRawType().equals(Optional.class);
+        return delegate.decode(response, type);
     }
 
 
