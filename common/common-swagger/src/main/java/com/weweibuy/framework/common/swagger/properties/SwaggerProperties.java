19bc4c7d481b8d3df4b9a8e5110c788ca6680988
杜仁浩 <ak514250@126.com>
2020-09-23 23:10:00 +0800

    swagger 相关

---
+++ common/common-swagger/src/main/java/com/weweibuy/framework/common/swagger/properties/SwaggerProperties.java
@@ -0,0 +1,50 @@
+package com.weweibuy.framework.common.swagger.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author durenhao
+ * @date 2020/7/21 18:29
+ **/
+@Data
+@ConfigurationProperties(prefix = "common.swagger")
+public class SwaggerProperties {
+
+    private String group = "swagger";
+
+    private String version = "v1";
+
+    private Set<String> basePackage = Collections.EMPTY_SET;
+
+    private List<SwaggerHeaderProperties> headers = new ArrayList<>();
+
+    private List<HttpStatusDescProperties> response = new ArrayList<>();
+
+    @Data
+    public static class HttpStatusDescProperties {
+
+        private Integer status;
+
+        private String desc;
+
+    }
+
+    @Data
+    public static class SwaggerHeaderProperties {
+
+        private String name = "";
+
+        private String desc = "";
+
+        private Boolean required = false;
+
+    }
+
+
+}
