5a0c2430699764a10f698e63599fa79a02110a75
suxing.zhang <jeresuxing@163.com>
2020-10-18 22:26:27 +0800

    state-machine简单demo，适用于订单的状态变更场景

--- samples/src/main/java/com/weweibuy/framework/samples/state/OrderCreateAction.java
+++ samples/src/main/java/com/weweibuy/framework/samples/state/OrderCreateAction.java
@@ -1,6 +1,7 @@
 package com.weweibuy.framework.samples.state;
 
 import com.weweibuy.framerwork.statemachine.provider.StateAction;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,10 +9,14 @@ import org.springframework.stereotype.Component;
  * @date : 2020/10/18 3:54 下午
  */
 @Component
+@RequiredArgsConstructor
 public class OrderCreateAction extends StateAction {
+    private final OrderService orderService;
 
+    // 可在此处扩张自己的业务逻辑 在业务需要校验 持久层数据状态是否与入口一致
     @Override
     protected Object doAction(Object data, String event, String source, String target) {
+//        return orderService.createNumber();
         return null;
     }
 
---
+++ samples/src/main/java/com/weweibuy/framework/samples/state/OrderDto.java
@@ -0,0 +1,43 @@
+package com.weweibuy.framework.samples.state;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author : Knight
+ * @date : 2020/10/18 10:12 下午
+ */
+@Getter
+@Setter
+@Builder
+public class OrderDto {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 状态
+     */
+    private String orderStatus;
+
+    /**
+     * 单据号
+     */
+    private String orderNo;
+
+    /**
+     * 创建时间
+     */
+    @Builder.Default
+    private LocalDateTime createTime = LocalDateTime.now();
+
+    /**
+     * 更新时间
+     */
+    @Builder.Default
+    private LocalDateTime updateTime = LocalDateTime.now();
+}
