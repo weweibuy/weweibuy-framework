b299be822a449a88e0379f7170544c95668954c8
suxing.zhang <suxing.zhang@pousheng.com>
2020-10-19 21:15:14 +0800

    状态及代码提交

--- samples/src/main/java/com/weweibuy/framework/samples/state/OrderCreateAction.java
+++ samples/src/main/java/com/weweibuy/framework/samples/state/OrderCreateAction.java
@@ -1,6 +1,6 @@
 package com.weweibuy.framework.samples.state;
 
-import com.weweibuy.framerwork.statemachine.provider.StateAction;
+import com.weweibuy.framerwork.statemachine.provider.AbstractStateAction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +10,15 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @RequiredArgsConstructor
-public class OrderCreateAction extends StateAction {
+public class OrderCreateAction extends AbstractStateAction {
     private final OrderService orderService;
 
-    // 可在此处扩张自己的业务逻辑 在业务需要校验 持久层数据状态是否与入口一致
+    /**
+     * 可在此处扩张自己的业务逻辑 在业务需要校验 持久层数据状态是否与入口一致
+     */
     @Override
     protected Object doAction(Object data, String event, String source, String target) {
-        return orderService.createNumber((String) data, target);
+        return orderService.createOrder((String) data, target);
     }
 
 
