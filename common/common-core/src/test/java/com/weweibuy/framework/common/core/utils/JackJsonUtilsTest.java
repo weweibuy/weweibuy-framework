d0e234e58ef9206d3d9662de35a5d50988313513
杜仁浩 <ak514250@126.com>
2020-12-12 21:15:11 +0800

    jackson utils

---
+++ common/common-core/src/test/java/com/weweibuy/framework/common/core/utils/JackJsonUtilsTest.java
@@ -0,0 +1,11 @@
+package com.weweibuy.framework.common.core.utils;
+
+import org.junit.Test;
+
+public class JackJsonUtilsTest {
+
+    @Test
+    public void readSnakeCaseValue() throws Exception {
+    }
+
+}
