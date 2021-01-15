bc6c1dbab5de3ee2e83df00d9c6e41e009ecfd5b
durenhao <durenhao@yunquna.com>
2021-01-15 17:33:21 +0800

    csv 工具

--- common/common-util/common-util-csv/src/main/java/com/weweibuy/framework/common/util/csv/ReflectCsvContentConverter.java
+++ common/common-util/common-util-csv/src/main/java/com/weweibuy/framework/common/util/csv/ReflectCsvContentConverter.java
@@ -1,7 +1,9 @@
 package com.weweibuy.framework.common.util.csv;
 
 import com.weweibuy.framework.common.util.csv.annotation.CsvProperty;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
+import org.springframework.cglib.beans.BulkBean;
 
 import java.lang.reflect.Field;
 import java.util.*;
@@ -17,38 +19,47 @@ public class ReflectCsvContentConverter<T> implements CsvContentConverter<T> {
 
     private final Class<? extends T> type;
 
-    private Integer[] indexArr;
+    private String[] indexName;
 
     private String[] header;
+
+    private BulkBean bulkBean;
 
     public ReflectCsvContentConverter(Class<? extends T> type) {
         this.type = type;
         init();
     }
 
-
     private void init() {
-        // a 1;   b  0;  c  3  -->  b a c    0 b      1 a     2 c
         Field[] fieldsWithAnnotation = FieldUtils.getFieldsWithAnnotation(type, CsvProperty.class);
-
-        AtomicInteger arrAtomicInteger = new AtomicInteger(0);
 
         AtomicInteger sortAtomicInteger = new AtomicInteger(0);
 
         header = new String[fieldsWithAnnotation.length];
-        indexArr = new Integer[fieldsWithAnnotation.length];
+        indexName = new String[fieldsWithAnnotation.length];
 
         // 数组中索引
-        Map<Field, Integer> arrFieldIndexMap = Arrays.stream(fieldsWithAnnotation)
-                .collect(Collectors.toMap(Function.identity(), f -> arrAtomicInteger.getAndIncrement()));
+        Map<Field, String> arrFieldIndexMap = Arrays.stream(fieldsWithAnnotation)
+                .collect(Collectors.toMap(Function.identity(), f -> f.getName()));
 
+        String[] getters = new String[fieldsWithAnnotation.length];
+        String[] setters = new String[fieldsWithAnnotation.length];
+        Class[] types = new Class[fieldsWithAnnotation.length];
 
         Arrays.stream(fieldsWithAnnotation)
                 .sorted(Comparator.comparing(field -> field.getAnnotation(CsvProperty.class).order()))
-                .peek(field -> header[sortAtomicInteger.get()] = field.getAnnotation(CsvProperty.class).name())
-                .forEach(field -> indexArr[sortAtomicInteger.getAndIncrement()] = arrFieldIndexMap.get(field));
+                .peek(field -> header[sortAtomicInteger.get()] =
+                        Optional.ofNullable(field.getAnnotation(CsvProperty.class).name())
+                                .filter(StringUtils::isNotBlank)
+                                .orElse(field.getName()))
+                .peek(field -> indexName[sortAtomicInteger.get()] = arrFieldIndexMap.get(field))
+                .peek(field -> getters[sortAtomicInteger.get()] = fieldGetter(field))
+                .peek(field -> setters[sortAtomicInteger.get()] = fieldSetter(field))
+                .forEach(field -> types[sortAtomicInteger.getAndIncrement()] = field.getType());
 
+        bulkBean = BulkBean.create(type, getters, setters, types);
     }
+
 
     @Override
     public Collection<String[]> convert(String[] header, List<T> body) {
@@ -62,23 +73,28 @@ public class ReflectCsvContentConverter<T> implements CsvContentConverter<T> {
 
 
     private String[] oneLine(T t) {
-        Field[] fieldsWithAnnotation = FieldUtils.getFieldsWithAnnotation(t.getClass(), CsvProperty.class);
+        Object[] propertyValues = bulkBean.getPropertyValues(t);
         String[] strings = new String[header.length];
         for (int i = 0; i < header.length; i++) {
-            Field field = fieldsWithAnnotation[indexArr[i]];
-            strings[i] = fieldValue(field, t);
+            Object o = propertyValues[i];
+            strings[i] = o.toString();
         }
         return strings;
     }
 
 
-    private String fieldValue(Field field, T t) {
-        try {
-            return FieldUtils.readField(field, t, true).toString();
-        } catch (IllegalAccessException e) {
-            throw new IllegalStateException(e);
-        }
+    private static String fieldGetter(Field field) {
+        return "get" + captureName(field.getName());
+    }
 
+    private static String fieldSetter(Field field) {
+        return "set" + captureName(field.getName());
+    }
+
+    private static String captureName(String str) {
+        char[] cs = str.toCharArray();
+        cs[0] -= 32;
+        return String.valueOf(cs);
     }
 
 
--- common/common-util/common-util-csv/src/main/java/com/weweibuy/framework/common/util/csv/Student.java
+++ common/common-util/common-util-csv/src/main/java/com/weweibuy/framework/common/util/csv/Student.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 /**
  * @author : Knight
  * @date : 2021/1/14 11:55 上午
@@ -22,5 +24,8 @@ public class Student {
 
     @CsvProperty(name = "学校", order = 1)
     private String school;
+
+    @CsvProperty(name = "生日", order = 80)
+    private LocalDateTime birthday;
 
 }
