8c88a137c95bc8203f91afa8fad5dc628c991b90
杜仁浩 <ak514250@126.com>
2021-01-17 20:10:22 +0800

    csv 工具 转化

--- common/common-util/common-util-csv/src/test/java/com/weweibuy/framework/common/util/csv/CsvUtilsTest.java
+++ common/common-util/common-util-csv/src/test/java/com/weweibuy/framework/common/util/csv/CsvUtilsTest.java
@@ -1,27 +1,36 @@
 package com.weweibuy.framework.common.util.csv;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weweibuy.framework.common.core.model.constant.CommonConstant;
+import lombok.Data;
 import org.junit.Test;
-import org.springframework.cglib.beans.BeanMap;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.charset.Charset;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 public class CsvUtilsTest {
 
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void export() throws Exception {
-        ReflectCsvContentConverter<Student> converter = new ReflectCsvContentConverter<>(Student.class);
-        Student student1 = new Student(1, "tom", "A", LocalDateTime.now());
-        Student student2 = new Student(2, "jack", null, LocalDateTime.now());
+        Student student1 = new Student("1班", "清华");
+        student1.setName("tom");
+        student1.setBirthday(LocalDate.now());
+
+
+        Student student2 = new Student("2班", "北大");
+        student2.setAge(13);
+        student2.setName("jack");
+        student2.setBirthday(LocalDate.now());
+
         List<Student> students = Arrays.asList(student1, student2);
         FileOutputStream fileOutputStream = new FileOutputStream("C:/Users/z/Desktop/tmp/test.csv");
-        CsvUtils.export(null, students, converter, fileOutputStream, Charset.forName("GBK"));
+        CsvUtils.export( students, Student.class, fileOutputStream, Charset.forName("GBK"));
 
     }
 
@@ -30,27 +39,31 @@ public class CsvUtilsTest {
         FileInputStream fileOutputStream =
                 new FileInputStream("C:/Users/z/Desktop/tmp/test.csv");
         List<Student> read =
-                CsvUtils.read(Student.class, fileOutputStream, CommonConstant.CharsetConstant.GBK, false);
+                CsvUtils.read(Student.class, fileOutputStream, CommonConstant.CharsetConstant.GBK, true);
         System.err.println(read);
 
     }
 
-
-
-
-
     @Test
-    public void test() {
-        Student student1 = new Student(1, "tom", "A", LocalDateTime.now());
-        Student student2 = new Student(2, "jack", "b", LocalDateTime.now());
-        BeanMap beanMap = BeanMap.create(student1);
-        Object name = beanMap.get("name");
-        System.err.println(name);
-        beanMap.setBean(student2);
-        Object name2 = beanMap.get("name");
-        System.err.println(name2);
-        BeanMap beanMap2 = BeanMap.create(student2);
-        System.err.println(beanMap.equals(beanMap2));
+    public void test1() throws Exception {
+        User user = new User();
+        int age = user.getAge();
+        char aChar = '\u0000';
+        System.err.println(aChar);
     }
+
+
+    @Data
+    static class User {
+
+        private int age;
+
+        private long id;
+
+        private char aChar;
+
+    }
+
+
 
 }
---
+++ common/common-util/common-util-csv/src/test/java/com/weweibuy/framework/common/util/csv/Person.java
@@ -0,0 +1,24 @@
+package com.weweibuy.framework.common.util.csv;
+
+import com.weweibuy.framework.common.util.csv.annotation.CsvProperty;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * @author durenhao
+ * @date 2021/1/17 11:33
+ **/
+@Data
+public class Person {
+
+    @CsvProperty(name = "年龄")
+    private int age;
+
+    @CsvProperty(name = "姓名")
+    private String name;
+
+    @CsvProperty(name = "生日", converter = LocalDateCsvTypeConverter.class)
+    private LocalDate birthday;
+
+}
