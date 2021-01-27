2309ea32d3858a4b0bcc104fdc75d6b1df816dd1
durenhao <durenhao@yunquna.com>
2021-01-27 14:08:23 +0800

    补偿 jackson 对象转化

--- compensate/core/src/main/java/com/weweibuy/framework/compensate/support/JackJsonMethodConverter.java
+++ compensate/core/src/main/java/com/weweibuy/framework/compensate/support/JackJsonMethodConverter.java
@@ -39,6 +39,8 @@ public class JackJsonMethodConverter implements MethodArgsConverter {
             }
             Object[] objects = new Object[list.size()];
             for (int i = 0; i < list.size(); i++) {
+//                objects[i] = objectMapper.convertValue(list.get(i), methodArgsTypeHolder.getType(generateKey(compensateInfo.getCompensateKey(), i)));
+
                 String string = objectMapper.writeValueAsString(list.get(i));
                 Object value = objectMapper.readValue(string, methodArgsTypeHolder.getType(generateKey(compensateInfo.getCompensateKey(), i)));
                 objects[i] = value;
