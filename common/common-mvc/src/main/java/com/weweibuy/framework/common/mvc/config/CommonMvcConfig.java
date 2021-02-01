1afd7c0f90c6a853e9fdd631af61efbe8fdb7ff1
杜仁浩 <ak514250@126.com>
2021-02-01 20:15:54 +0800

    部分配置修改

--- common/common-mvc/src/main/java/com/weweibuy/framework/common/mvc/config/CommonMvcConfig.java
+++ common/common-mvc/src/main/java/com/weweibuy/framework/common/mvc/config/CommonMvcConfig.java
@@ -1,10 +1,5 @@
 package com.weweibuy.framework.common.mvc.config;
 
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.weweibuy.framework.common.core.model.constant.CommonConstant;
 import com.weweibuy.framework.common.core.support.SystemIdGetter;
 import com.weweibuy.framework.common.mvc.advice.CommonErrorAttributes;
 import com.weweibuy.framework.common.mvc.advice.CommonExceptionAdvice;
@@ -14,13 +9,10 @@ import com.weweibuy.framework.common.mvc.endpoint.CustomHealthController;
 import com.weweibuy.framework.common.mvc.support.DefaultFeignExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -66,21 +58,6 @@ public class CommonMvcConfig {
     }
 
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer localDateTimeJackson2ObjectMapperBuilderCustomizer() {
-        return builder ->
-                builder.serializerByType(LocalDateTime.class, localDateTimeSerializer())
-                        .deserializerByType(LocalDateTime.class, localDateTimeDeserializer());
-
-    }
-
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer localDateJackson2ObjectMapperBuilderCustomizer() {
-        return builder ->
-                builder.serializerByType(LocalDate.class, localDateSerializer())
-                        .deserializerByType(LocalDate.class, localDateDeserializer());
-    }
-
-    @Bean
     @ConditionalOnClass(name = {"feign.Feign", "com.weweibuy.framework.common.core.exception.MethodKeyFeignException"})
     public DefaultFeignExceptionHandler defaultFeignExceptionHandler() {
         return new DefaultFeignExceptionHandler(feignMethodKeyMappingConverterList);
@@ -91,20 +68,4 @@ public class CommonMvcConfig {
         return new HealthLogDisableConfigurer();
     }
 
-
-    public LocalDateTimeSerializer localDateTimeSerializer() {
-        return new LocalDateTimeSerializer(CommonConstant.DateConstant.STANDARD_DATE_TIME_FORMATTER);
-    }
-
-    public LocalDateTimeDeserializer localDateTimeDeserializer() {
-        return new LocalDateTimeDeserializer(CommonConstant.DateConstant.STANDARD_DATE_TIME_FORMATTER);
-    }
-
-    public LocalDateSerializer localDateSerializer() {
-        return new LocalDateSerializer(CommonConstant.DateConstant.STANDARD_DATE_FORMATTER);
-    }
-
-    public LocalDateDeserializer localDateDeserializer() {
-        return new LocalDateDeserializer(CommonConstant.DateConstant.STANDARD_DATE_FORMATTER);
-    }
 }
