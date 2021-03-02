a4902d1f353ccc95a938a0e77ffa13f39a90178b
durenhao <durenhao@yunquna.com>
2021-03-02 20:03:23 +0800

    ES

--- common/common-es/src/main/java/com/weweibuy/framework/common/es/config/CommonElasticsearchConfig.java
+++ common/common-es/src/main/java/com/weweibuy/framework/common/es/config/CommonElasticsearchConfig.java
@@ -1,16 +1,16 @@
-/*
- * All rights Reserved, Designed By baowei
- *
- * 注意：本内容仅限于内部传阅，禁止外泄以及用于其他的商业目的
- */
-package com.weweibuy.framework.samples.config;
+package com.weweibuy.framework.common.es.config;
 
+import com.weweibuy.framework.common.es.convert.CustomerMappingElasticsearchConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
 import org.springframework.data.elasticsearch.core.convert.MappingElasticsearchConverter;
 import org.springframework.data.elasticsearch.core.mapping.ElasticsearchPersistentEntity;
 import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMappingContext;
 
 /**
+ * ES 配置
+ *
  * springBoot 2.3+ 版本使用的 ES 文档与Java对象的转化器是 {@link  MappingElasticsearchConverter}
  * 使用 {@link MappingElasticsearchConverter#readProperties(ElasticsearchPersistentEntity, Object, MappingElasticsearchConverter.ElasticsearchPropertyValueProvider)}
  * 进行转化
@@ -23,12 +23,12 @@ import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMa
  * @date 2019/8/12 20:35
  **/
 @Configuration
-public class EsConfig {
+public class CommonElasticsearchConfig {
 
-//    @Bean
-//    public EntityMapper entityMapper(SimpleElasticsearchMappingContext mappingContext, Jackson2ObjectMapperBuilder objectMapperBuilder) {
-//        return new SnackCaseEsEntityMapper(mappingContext, objectMapperBuilder);
-//    }
+    @Bean
+    public ElasticsearchConverter elasticsearchConverter(SimpleElasticsearchMappingContext mappingContext) {
+        return new CustomerMappingElasticsearchConverter(mappingContext);
+    }
 
 
 }
---
+++ common/common-es/src/main/java/com/weweibuy/framework/common/es/convert/CustomerMappingElasticsearchConverter.java
@@ -0,0 +1,30 @@
+package com.weweibuy.framework.common.es.convert;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.weweibuy.framework.common.core.utils.JackJsonUtils;
+import org.springframework.data.elasticsearch.core.convert.MappingElasticsearchConverter;
+import org.springframework.data.elasticsearch.core.mapping.ElasticsearchPersistentEntity;
+import org.springframework.data.elasticsearch.core.mapping.ElasticsearchPersistentProperty;
+import org.springframework.data.mapping.context.MappingContext;
+
+import java.util.Map;
+
+/**
+ * @author durenhao
+ * @date 2021/3/2 16:20
+ **/
+public class CustomerMappingElasticsearchConverter extends MappingElasticsearchConverter {
+
+    public CustomerMappingElasticsearchConverter(MappingContext<? extends ElasticsearchPersistentEntity<?>, ElasticsearchPersistentProperty> mappingContext) {
+        super(mappingContext);
+    }
+
+    @Override
+    protected <R> R readProperties(Class<R> type, Map<String, Object> map) {
+        ObjectMapper snakeCaseMapper = JackJsonUtils.getSnakeCaseMapper();
+        return snakeCaseMapper.convertValue(map, type);
+    }
+
+
+
+}
