0d9ae762e3cdd78a4b773f98b2874837b6ba3b69
durenhao <durenhao@yunquna.com>
2021-03-02 15:29:53 +0800

    ES

--- samples/src/main/java/com/weweibuy/framework/samples/config/EsConfig.java
+++ samples/src/main/java/com/weweibuy/framework/samples/config/EsConfig.java
@@ -8,11 +8,14 @@ package com.weweibuy.framework.samples.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.convert.MappingElasticsearchConverter;
 import org.springframework.data.elasticsearch.core.mapping.ElasticsearchPersistentEntity;
+import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMappingContext;
 
 /**
  * springBoot 2.3+ 版本使用的 ES 文档与Java对象的转化器是 {@link  MappingElasticsearchConverter}
  * 使用 {@link MappingElasticsearchConverter#readProperties(ElasticsearchPersistentEntity, Object, MappingElasticsearchConverter.ElasticsearchPropertyValueProvider)}
  * 进行转化
+ * 自动配置 {@link ElasticsearchDataConfiguration.BaseConfiguration#elasticsearchConverter(SimpleElasticsearchMappingContext)}
+ *
  * <p>
  * springBoot 2.3 版本之前使用的是  EntityMapper
  *
