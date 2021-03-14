bb9f7368b1316c44ccc75f31f8f414e1acb3375b
杜仁浩 <ak514250@126.com>
2021-03-14 21:08:23 +0800

    lb 配置

--- load-balance/src/main/java/com/weweibuy/framework/lb/config/CustomLoadBalancerConfig.java
+++ load-balance/src/main/java/com/weweibuy/framework/lb/config/CustomLoadBalancerConfig.java
@@ -1,9 +1,7 @@
 package com.weweibuy.framework.lb.config;
 
 import com.weweibuy.framework.lb.endpoint.LoadBalanceEndpoint;
-import com.weweibuy.framework.lb.mq.RocketServerChangeListener;
 import com.weweibuy.framework.lb.support.LoadBalanceOperator;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,11 +19,6 @@ public class CustomLoadBalancerConfig {
         return new LoadBalanceEndpoint(loadBalanceOperator());
     }
 
-    @Bean
-    @ConditionalOnBean(type = "com.weweibuy.framework.rocketmq.core.consumer.RocketBeanPostProcessor")
-    public RocketServerChangeListener rocketServerChangeListener() {
-        return new RocketServerChangeListener(loadBalanceOperator());
-    }
 
     @Bean
     public LoadBalanceOperator loadBalanceOperator() {
