8cd3fc8173665511ab2452bf3ef053c569fefe81
杜仁浩 <ak514250@126.com>
2021-04-17 21:49:44 +0800

    feign 上文件

--- common/common-feign/src/main/java/com/weweibuy/framework/common/feign/support/MultipartFileHelper.java
+++ common/common-feign/src/main/java/com/weweibuy/framework/common/feign/support/MultipartFileHelper.java
@@ -5,13 +5,11 @@ import lombok.NoArgsConstructor;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 
 /**
@@ -41,6 +39,28 @@ public class MultipartFileHelper {
         try (FileInputStream fileInputStream = new FileInputStream(file);
              OutputStream outputStream = fileItem.getOutputStream()) {
             IOUtils.copy(fileInputStream, outputStream);
+        }
+        return new CommonsMultipartFile(fileItem);
+    }
+
+    /**
+     * 通过流创建 MultipartFile
+     *
+     * @param inputStream
+     * @param fileName
+     * @return
+     * @throws IOException
+     */
+    public static MultipartFile createMultipartFile(InputStream inputStream, String fileName) throws IOException {
+        int available = inputStream.available();
+        FileItem fileItem = new DiskFileItem("file",
+                MediaType.TEXT_PLAIN_VALUE,
+                false,
+                fileName,
+                available,
+                null);
+        try (OutputStream outputStream = fileItem.getOutputStream()) {
+            IOUtils.copy(inputStream, outputStream);
         }
         return new CommonsMultipartFile(fileItem);
     }
