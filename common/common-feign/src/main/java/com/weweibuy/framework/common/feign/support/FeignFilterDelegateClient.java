1525f76a6455bbd842a83bd97b02f98a268065a5
杜仁浩 <ak514250@126.com>
2021-11-01 00:16:49 +0800

    feign mock 新方式

--- common/common-feign/src/main/java/com/weweibuy/framework/common/feign/support/FeignFilterDelegateClient.java
+++ common/common-feign/src/main/java/com/weweibuy/framework/common/feign/support/FeignFilterDelegateClient.java
@@ -3,6 +3,7 @@ package com.weweibuy.framework.common.feign.support;
 import feign.Client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
  * @author durenhao
  * @date 2021/10/30 17:21
  **/
+@Order(0)
 @RequiredArgsConstructor
 public class FeignFilterDelegateClient implements DelegateFeignClient {
 
