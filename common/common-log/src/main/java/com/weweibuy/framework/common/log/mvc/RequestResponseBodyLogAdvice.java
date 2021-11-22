782e2cddca8e5ab87f0928fb85fe4e7e3a25311a
杜仁浩 <ak514250@126.com>
2021-11-22 12:26:04 +0800

    日志 2

--- common/common-log/src/main/java/com/weweibuy/framework/common/log/mvc/RequestResponseBodyLogAdvice.java
+++ common/common-log/src/main/java/com/weweibuy/framework/common/log/mvc/RequestResponseBodyLogAdvice.java
@@ -6,15 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpInputMessage;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,7 +23,7 @@ import java.lang.reflect.Type;
  **/
 @Slf4j
 @ControllerAdvice
-public class RequestResponseBodyLogAdvice implements RequestBodyAdvice, ResponseBodyAdvice {
+public class RequestResponseBodyLogAdvice implements RequestBodyAdvice {
 
     @Autowired(required = false)
     private ReadableBodyRequestHandler readableBodyRequestHandler;
@@ -61,24 +57,6 @@ public class RequestResponseBodyLogAdvice implements RequestBodyAdvice, Response
 
     @Override
     public Object handleEmptyBody(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
-        return body;
-    }
-
-
-    // ================ ResponseBodyAdvice =================
-
-    @Override
-    public boolean supports(MethodParameter returnType, Class converterType) {
-        return true;
-    }
-
-    @Override
-    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        try {
-            HttpLogger.logResponse(body, selectedContentType);
-        } catch (Exception e) {
-            log.warn("输出响应日志异常: {}", e.getMessage());
-        }
         return body;
     }
 
