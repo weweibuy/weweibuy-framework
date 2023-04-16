e5d0213f1dc569b1897829f6fc71600834af0305
杜仁浩 <ak514250@126.com>
2023-04-16 12:00:44 +0800

    同步代码

--- common/common-log/src/main/java/com/weweibuy/framework/common/log/mvc/MvcLogRequestHandler.java
+++ common/common-log/src/main/java/com/weweibuy/framework/common/log/mvc/MvcLogRequestHandler.java
@@ -3,11 +3,11 @@ package com.weweibuy.framework.common.log.mvc;
 import com.weweibuy.framework.common.core.support.ReadableBodyRequestHandler;
 import com.weweibuy.framework.common.log.config.CommonLogProperties;
 import com.weweibuy.framework.common.log.logger.HttpLogger;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Optional;
 
