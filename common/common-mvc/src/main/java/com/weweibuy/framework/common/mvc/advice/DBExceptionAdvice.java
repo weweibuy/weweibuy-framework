f2e40d18f7bbf7b2ae2f56f47c0dd765d272a814
杜仁浩 <ak514250@126.com>
2023-06-10 23:01:32 +0800

    代码格式
    (cherry picked from commit 565d71fc9cb5d357d30bc9159a2fae102cde1fe5)

--- common/common-mvc/src/main/java/com/weweibuy/framework/common/mvc/advice/DBExceptionAdvice.java
+++ common/common-mvc/src/main/java/com/weweibuy/framework/common/mvc/advice/DBExceptionAdvice.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
 
 /**
