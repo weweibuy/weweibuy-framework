0e6a832e8ab7e8e906eeef790c602b53ba16e657
杜仁浩 <ak514250@126.com>
2023-06-10 21:37:56 +0800

    日志获取优化

--- common/common-feign/src/main/java/com/weweibuy/framework/common/feign/support/FeignResponseUtils.java
+++ common/common-feign/src/main/java/com/weweibuy/framework/common/feign/support/FeignResponseUtils.java
@@ -1,11 +1,15 @@
 package com.weweibuy.framework.common.feign.support;
 
+import com.weweibuy.framework.common.core.model.constant.CommonConstant;
 import feign.Response;
 import feign.Util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 /**
  * feign 响应工具
@@ -24,23 +28,56 @@ public final class FeignResponseUtils {
      * @throws IOException
      */
     public static String responseBody(Response response) throws IOException {
+        return responseBody(response, CommonConstant.CharsetConstant.UT8);
+    }
+
+    /**
+     * 响应体
+     *
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    public static String responseBody(Response response, Charset charset) throws IOException {
         int status = response.status();
         Response.Body body = response.body();
         String bodyStr = "";
         if (body != null && !(status == 204 || status == 205)) {
             byte[] bodyData = Util.toByteArray(body.asInputStream());
-            bodyStr = new String(bodyData);
+            bodyStr = new String(bodyData, charset);
         }
         return bodyStr;
     }
 
 
-    public static Response reeBufferResponse(Response response, byte[] bodyData) {
+    public static Response rebufferResponse(Response response, byte[] bodyData) {
         Response.Body body = response.body();
         if (!body.isRepeatable()) {
             response = response.toBuilder().body(bodyData).build();
         }
         return response;
+    }
+
+    /**
+     * 获取响应体, 并 reBuffer 响应体
+     *
+     * @param response
+     * @param charset
+     * @return
+     * @throws IOException
+     */
+    public static Pair<Response, String> bodyAndReBufferResponse(Response response, Charset charset) throws IOException {
+        int status = response.status();
+        String bodyStr = "";
+        Response.Body body = response.body();
+        if (body != null && !(status == 204 || status == 205)) {
+            byte[] bodyData = Util.toByteArray(body.asInputStream());
+            bodyStr = new String(bodyData, charset);
+            if (!body.isRepeatable()) {
+                response = response.toBuilder().body(bodyData).build();
+            }
+        }
+        return Pair.of(response, bodyStr);
     }
 
 }
