f602ecbf8bc8903e854ad14b56ff397b29666ba0
杜仁浩 <ak514250@126.com>
2023-07-01 21:29:45 +0800

    rsa

--- common/common-codec/src/main/java/com/weweibuy/framework/common/codec/rsa/RsaKeyHelper.java
+++ common/common-codec/src/main/java/com/weweibuy/framework/common/codec/rsa/RsaKeyHelper.java
@@ -12,23 +12,30 @@
  */
 package com.weweibuy.framework.common.codec.rsa;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.bouncycastle.asn1.ASN1Sequence;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.UncheckedIOException;
+import java.io.*;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.*;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Enumeration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -43,8 +50,135 @@ public class RsaKeyHelper {
     private static Charset UTF8 = StandardCharsets.UTF_8;
 
     private static String BEGIN = "-----BEGIN";
+
     private static Pattern PEM_DATA = Pattern.compile("-----BEGIN (.*)-----(.*)-----END (.*)-----", Pattern.DOTALL);
 
+    private static final Pattern SSH_PUB_KEY = Pattern.compile("ssh-(rsa|dsa) ([A-Za-z0-9/+]+=*) (.*)");
+
+
+    /**
+     * 生成二进制秘钥
+     *
+     * @param publicKeyOutput
+     * @param privateKeyOutput
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     */
+    public static void generateBinaryKeyToFile(String publicKeyOutput, String privateKeyOutput) throws NoSuchAlgorithmException, IOException {
+        generateBinaryKeyToFile(publicKeyOutput, privateKeyOutput, 2048);
+    }
+
+    /**
+     * 生成秘钥对
+     *
+     * @param keySize
+     * @return
+     * @throws NoSuchAlgorithmException
+     */
+    public static KeyPair generateKeyPair(Integer keySize) throws NoSuchAlgorithmException {
+        final KeyPairGenerator keyGen = KeyPairGenerator.getInstance(RSAUtils.ALGORITHM);
+        keyGen.initialize(keySize);
+        return keyGen.generateKeyPair();
+    }
+
+    /**
+     * 生成一份二进制的秘钥
+     *
+     * @param publicKeyOutput
+     * @param privateKeyOutput
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     */
+    public static void generateBinaryKeyToFile(String publicKeyOutput, String privateKeyOutput, Integer keySize) throws NoSuchAlgorithmException, IOException {
+        KeyPair key = generateKeyPair(keySize);
+
+        try (DataOutputStream dos = new DataOutputStream(FileUtils.openOutputStream(new File(publicKeyOutput), false))) {
+            dos.write(key.getPublic().getEncoded());
+        }
+        try (DataOutputStream dos = new DataOutputStream(FileUtils.openOutputStream(new File(privateKeyOutput), false))) {
+            dos.write(key.getPrivate().getEncoded());
+        }
+    }
+
+    /**
+     * 生成 base64 编码的秘钥
+     *
+     * @param publicKeyOutput
+     * @param privateKeyOutput
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     */
+    public static void generateBase64KeyToFile(String publicKeyOutput, String privateKeyOutput) throws NoSuchAlgorithmException, IOException {
+        generateBase64KeyToFile(publicKeyOutput, privateKeyOutput, 2048);
+    }
+
+    /**
+     * 生成base64的秘钥
+     *
+     * @param publicKeyOutput
+     * @param privateKeyOutput
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     */
+    public static void generateBase64KeyToFile(String publicKeyOutput, String privateKeyOutput, Integer keySize) throws NoSuchAlgorithmException, IOException {
+        KeyPair key = generateKeyPair(keySize);
+
+        try (DataOutputStream dos = new DataOutputStream(FileUtils.openOutputStream(new File(publicKeyOutput), false))) {
+            dos.write(Base64.getEncoder().encode(key.getPublic().getEncoded()));
+        }
+        try (DataOutputStream dos = new DataOutputStream(FileUtils.openOutputStream(new File(privateKeyOutput), false))) {
+            dos.write(Base64.getEncoder().encode(key.getPrivate().getEncoded()));
+        }
+    }
+
+    /**
+     * 生成 base64 key
+     *
+     * @return 数组0:  公钥;  数组1: 私钥
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     */
+    public static String[] generateKeyToBase64Str(Integer keySize) throws NoSuchAlgorithmException {
+        KeyPair key = generateKeyPair(keySize);
+
+        byte[] publicKey = Base64.getEncoder().encode(key.getPublic().getEncoded());
+        byte[] privateKey = Base64.getEncoder().encode(key.getPrivate().getEncoded());
+        return new String[]{new String(publicKey), new String(privateKey)};
+    }
+
+
+    /**
+     * 从 pem秘钥文件流中, 解析秘钥
+     *
+     * @param inputStream
+     * @return
+     * @throws IOException
+     */
+    public static KeyPair parseKeyPair(InputStream inputStream) throws IOException {
+        String pemData = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        return parseKeyPair(pemData);
+    }
+
+    /**
+     * 从pem秘钥解析秘钥
+     *
+     * @param filePath
+     * @return
+     */
+    public static KeyPair parseKeyPairFromFile(String filePath) {
+        try (InputStream inputStream = new FileInputStream(filePath)) {
+            return parseKeyPair(inputStream);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 秘钥文件字符中解析秘钥
+     *
+     * @param pemData
+     * @return
+     */
     public static KeyPair parseKeyPair(String pemData) {
         Matcher m = PEM_DATA.matcher(pemData.trim());
 
@@ -106,7 +240,7 @@ public class RsaKeyHelper {
             throw new IllegalArgumentException(type + " is not a supported format");
         }
         byte[] content = Base64.getMimeDecoder().decode(utf8Encode(m.group(2)));
-        return RSAUtils.getPublicKeyFromBase64(content);
+        return getPublicKey(content);
     }
 
     /**
@@ -125,7 +259,7 @@ public class RsaKeyHelper {
             throw new IllegalArgumentException(type + " is not a supported format");
         }
         byte[] decode = Base64.getMimeDecoder().decode(utf8Encode(m.group(2)));
-        return RSAUtils.CertificateUtils.certificateFromByte(decode);
+        return CertificateHelper.certificateFromByte(decode);
     }
 
     /**
@@ -145,12 +279,17 @@ public class RsaKeyHelper {
             throw new IllegalArgumentException(type + " is not a supported format");
         }
         byte[] content = Base64.getMimeDecoder().decode(utf8Encode(m.group(2)));
-        return RSAUtils.getPrivateKeyFromP8(content);
+        return getPrivateKey(content);
     }
 
-    private static final Pattern SSH_PUB_KEY = Pattern.compile("ssh-(rsa|dsa) ([A-Za-z0-9/+]+=*) (.*)");
 
-    public static RSAPublicKey parsePublicKey(String key) {
+    /**
+     * 解析 ssh 公钥
+     *
+     * @param key
+     * @return
+     */
+    public static RSAPublicKey parseSSHPublicKey(String key) {
         Matcher m = SSH_PUB_KEY.matcher(key);
 
         if (m.matches()) {
@@ -162,10 +301,10 @@ public class RsaKeyHelper {
                 throw new IllegalArgumentException("Only RSA is currently supported, but algorithm was " + alg);
             }
 
-            return parseSSHPublicKey(encKey);
+            return parseSSHPublicKey0(encKey);
         } else if (!key.startsWith(BEGIN)) {
             // Assume it's the plain Base64 encoded ssh key without the "ssh-rsa" at the start
-            return parseSSHPublicKey(key);
+            return parseSSHPublicKey0(key);
         }
 
         KeyPair kp = parseKeyPair(key);
@@ -177,7 +316,7 @@ public class RsaKeyHelper {
         return (RSAPublicKey) kp.getPublic();
     }
 
-    private static RSAPublicKey parseSSHPublicKey(String encKey) {
+    private static RSAPublicKey parseSSHPublicKey0(String encKey) {
         final byte[] PREFIX = new byte[]{0, 0, 0, 7, 's', 's', 'h', '-', 'r', 's', 'a'};
         ByteArrayInputStream in = new ByteArrayInputStream(Base64.getMimeDecoder().decode(utf8Encode(encKey)));
         byte[] prefix = new byte[11];
@@ -233,5 +372,254 @@ public class RsaKeyHelper {
         }
     }
 
+
+    /**
+     * 获取公钥(二进制公钥)
+     *
+     * @param publicKeyPath 秘钥文件, 文件中只包含秘钥内容
+     * @return The {@link PublicKey} object.
+     * @throws Exception If there was an issue reading the file.
+     */
+    public static PublicKey getPublicKeyFromBinaryFile(String publicKeyPath) throws Exception {
+        return getPublicKey(Files.readAllBytes(Paths.get(publicKeyPath)));
+    }
+
+    /**
+     * 二进制流获取秘钥
+     *
+     * @param inputStream 只包含秘钥内容
+     * @return
+     * @throws Exception
+     */
+    public static PublicKey getPublicKeyFromBinaryStream(InputStream inputStream) throws Exception {
+        byte[] bytes = IOUtils.toByteArray(inputStream);
+        return getPublicKey(bytes);
+    }
+
+
+    /**
+     * 获取base64 公钥(公钥为 一行base64的文本, 没有换行)
+     *
+     * @param publicKeyPath 秘钥文件, 文件中只包含秘钥内容
+     * @return
+     * @throws Exception
+     */
+    public static PublicKey getPublicKeyFromBase64File(String publicKeyPath) throws Exception {
+        byte[] allBytes = Files.readAllBytes(Paths.get(publicKeyPath));
+        byte[] decode = Base64.getDecoder().decode(allBytes);
+        return getPublicKey(decode);
+    }
+
+    /**
+     * 获取base64 公钥(公钥为 一行base64的文本, 没有换行)
+     *
+     * @param publicKeyBase64Str 秘钥内容
+     * @return
+     * @throws Exception
+     */
+    public static PublicKey getPublicKeyFromBase64Str(String publicKeyBase64Str) throws Exception {
+        byte[] decode = Base64.getDecoder().decode(publicKeyBase64Str);
+        return getPublicKey(decode);
+    }
+
+    /**
+     * base64 获取公钥
+     *
+     * @param inputStream 只包含秘钥内容
+     * @return
+     * @throws Exception
+     */
+    public static PublicKey getPublicKeyFromBase64Stream(InputStream inputStream) throws Exception {
+        byte[] bytes = IOUtils.toByteArray(inputStream);
+        byte[] decode = Base64.getDecoder().decode(bytes);
+        return getPublicKey(decode);
+    }
+
+    /**
+     * 获取 私钥(二进制)
+     *
+     * @param privateKeyPath 只包含秘钥内容
+     * @return The {@link PrivateKey} object.
+     * @throws Exception If there was an issue reading the file.
+     */
+    public static PrivateKey getPrivateKeyFromBinaryFile(String privateKeyPath) throws Exception {
+        return getPrivateKey(Files.readAllBytes(Paths.get(privateKeyPath)));
+    }
+
+    /**
+     * 获取私钥
+     *
+     * @param privateKeyPath 只包含秘钥内容
+     * @return
+     * @throws Exception
+     */
+    public static PrivateKey getPrivateKeyFromBase64File(String privateKeyPath) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] bytes = Files.readAllBytes(Paths.get(privateKeyPath));
+        byte[] decode = Base64.getDecoder().decode(bytes);
+        return getPrivateKey(decode);
+
+    }
+
+    public static PrivateKey getPrivateKeyFromBase64Str(String base64Str) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] decode = Base64.getDecoder().decode(base64Str.getBytes());
+        return getPrivateKey(decode);
+    }
+
+    /**
+     * The method that will re-create a {@link PublicKey} from a public key byte array.
+     *
+     * @param encryptedPublicKey The byte array of a public key.
+     * @return The {@link PublicKey} object.
+     * @throws Exception If there was an issue reading the byte array.
+     */
+    public static PublicKey getPublicKey(byte[] encryptedPublicKey) throws Exception {
+        return KeyFactory.getInstance(RSAUtils.ALGORITHM).generatePublic(new X509EncodedKeySpec(encryptedPublicKey));
+    }
+
+    /**
+     * The method that will re-create a {@link PrivateKey} from a private key byte array.
+     *
+     * @param encryptedPrivateKey The array of bytes of a private key.
+     * @return The {@link PrivateKey} object.
+     * @throws Exception If there was an issue reading the byte array.
+     */
+    public static PrivateKey getPrivateKey(byte[] encryptedPrivateKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return KeyFactory.getInstance(RSAUtils.ALGORITHM).generatePrivate(new PKCS8EncodedKeySpec(encryptedPrivateKey));
+    }
+
+    /**
+     * 证书
+     */
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static final class CertificateHelper {
+
+        public static final String CERTIFICATE_TYPE = "X509";
+
+        /**
+         * 根据 cer文件流, 读取证书
+         *
+         * @param certificateFileStream
+         * @return
+         * @throws Exception
+         */
+        public static X509Certificate certificateFromStream(InputStream certificateFileStream) throws Exception {
+            String certificateText = IOUtils.toString(certificateFileStream, StandardCharsets.UTF_8);
+            return RsaKeyHelper.parseCertificate(certificateText);
+        }
+
+
+        /**
+         * 根据 cer文件流, 读取公钥
+         *
+         * @param certificateFileStream
+         * @return
+         * @throws Exception
+         */
+        public static PublicKey pubKeyFromCerStream(InputStream certificateFileStream) throws Exception {
+            return certificateFromStream(certificateFileStream).getPublicKey();
+        }
+
+        /**
+         * 根据 cer 文件获取证书
+         *
+         * @param filePath
+         * @return
+         * @throws Exception
+         */
+        public static X509Certificate certificateFromFile(String filePath) throws Exception {
+            try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+                return certificateFromStream(fileInputStream);
+            }
+        }
+
+        /**
+         * 根据 cer 文件获取公钥
+         *
+         * @param filePath
+         * @return
+         * @throws Exception
+         */
+        public static PublicKey pubKeyFromCerFile(String filePath) throws Exception {
+            return certificateFromFile(filePath).getPublicKey();
+        }
+
+
+        /**
+         * 根据公钥cer文本串读取证书,
+         *
+         * @param certificateBase64Text (不包含换行符)
+         * @return
+         */
+        public static X509Certificate certificateFromBase64Text(String certificateBase64Text) throws Exception {
+            return certificateFromByte(Base64.getDecoder().decode(certificateBase64Text.getBytes()));
+        }
+
+
+        /**
+         * 根据公钥cer文本串读取公钥,
+         *
+         * @param certificateBase64Text (不包含换行符)
+         * @return
+         */
+        public static PublicKey pubKeyFromCerBase64Text(String certificateBase64Text) throws Exception {
+            return certificateFromBase64Text(certificateBase64Text).getPublicKey();
+        }
+
+        public static X509Certificate certificateFromByte(byte[] certificateData) throws CertificateException {
+            CertificateFactory certificateFactory = CertificateFactory.getInstance(CERTIFICATE_TYPE);
+            return (X509Certificate) certificateFactory.generateCertificate(
+                    new ByteArrayInputStream(certificateData));
+        }
+
+        /**
+         * 根据私钥文件流读取私钥
+         *
+         * @param priKeyStream
+         * @param priKeyPass   私钥密码
+         * @return
+         */
+        public static PrivateKey privateKeyFromStream(InputStream priKeyStream, String priKeyPass) throws Exception {
+            byte[] reads = IOUtils.toByteArray(priKeyStream);
+            return privateKeyByStream(reads, priKeyPass);
+        }
+
+        /**
+         * 根据私钥文件读取私钥
+         *
+         * @param privateKeyPath
+         * @param priKeyPass     私钥密码
+         * @return
+         * @throws Exception
+         */
+        public static PrivateKey privateKeyFromFile(String privateKeyPath, String priKeyPass) throws Exception {
+            try (FileInputStream fileInputStream = new FileInputStream(privateKeyPath)) {
+                return privateKeyFromStream(fileInputStream, priKeyPass);
+            }
+        }
+
+        /**
+         * 根据PFX私钥字节流读取私钥
+         *
+         * @param pfxBytes
+         * @param priKeyPass 私钥密码
+         * @return
+         */
+        public static PrivateKey privateKeyByStream(byte[] pfxBytes, String priKeyPass) throws Exception {
+            KeyStore ks = KeyStore.getInstance("PKCS12");
+            if (priKeyPass == null) {
+                priKeyPass = "";
+            }
+            char[] charPriKeyPass = priKeyPass.toCharArray();
+            ks.load(new ByteArrayInputStream(pfxBytes), charPriKeyPass);
+
+            Enumeration<String> aliasEnum = ks.aliases();
+            String keyAlias = null;
+            if (aliasEnum.hasMoreElements()) {
+                keyAlias = aliasEnum.nextElement();
+            }
+            return (PrivateKey) ks.getKey(keyAlias, charPriKeyPass);
+        }
+
+    }
 
 }
