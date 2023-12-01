b3b5fe3e46a43c192814276b8d216d2c5a3db02d
durenhao <durenhao@g7.com.cn>
2023-12-01 18:07:49 +0800

    多数据源

---
+++ common/common-db/src/main/java/com/weweibuy/framework/common/db/annotation/SpecDataSource.java
@@ -0,0 +1,19 @@
+package com.weweibuy.framework.common.db.annotation;
+
+/**
+ * 指定数据源
+ *
+ * @author durenhao
+ * @date 2023/12/1 18:05
+ **/
+public @interface SpecDataSource {
+
+    /**
+     * 数据源名称
+     *
+     * @return
+     */
+    String value();
+
+
+}
