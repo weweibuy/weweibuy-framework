9a394fa8f0758d769ca36f8d3801a9f80a2ef98f
杜仁浩 <ak514250@126.com>
2023-12-02 20:53:34 +0800

    多数源指定

--- common/common-db/src/main/java/com/weweibuy/framework/common/db/config/CommonDBConfig.java
+++ common/common-db/src/main/java/com/weweibuy/framework/common/db/config/CommonDBConfig.java
@@ -4,12 +4,15 @@ import com.weweibuy.framework.common.db.aspect.SpecDatasourceAspect;
 import com.weweibuy.framework.common.db.aspect.SpecDatasourceBeanFactoryPointcutAdvisor;
 import com.weweibuy.framework.common.db.aspect.SpecDatasourcePointcut;
 import com.weweibuy.framework.common.db.encrypt.AESEncryptHelper;
+import com.weweibuy.framework.common.db.multiple.MultipleDatasourceAndMybatisRegister;
 import com.weweibuy.framework.common.db.properties.DBEncryptProperties;
 import com.weweibuy.framework.common.db.properties.MultipleDatasourceAndMybatisProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Role;
@@ -22,9 +25,6 @@ import org.springframework.context.annotation.Role;
 @EnableConfigurationProperties({DBEncryptProperties.class})
 public class CommonDBConfig {
 
-    @Autowired
-    private MultipleDatasourceAndMybatisProperties properties;
-
 
     @Bean
     @ConditionalOnProperty(name = "common.db.encrypt.enable", havingValue = "true")
@@ -32,16 +32,6 @@ public class CommonDBConfig {
         return new AESEncryptHelper();
     }
 
-    @Bean
-    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-    @ConditionalOnProperty(name = "common.db.multiple.enable-spec-datasource", havingValue = "true", matchIfMissing = true)
-    public SpecDatasourceBeanFactoryPointcutAdvisor compensateBeanFactoryPointcutAdvisor() {
-        Integer order = properties.getSpecDatasourceAspectOrder();
-        SpecDatasourceBeanFactoryPointcutAdvisor advisor = new SpecDatasourceBeanFactoryPointcutAdvisor();
-        advisor.setPc(new SpecDatasourcePointcut());
-        advisor.setOrder(order);
-        advisor.setAdvice(new SpecDatasourceAspect());
-        return advisor;
-    }
+
 
 }
