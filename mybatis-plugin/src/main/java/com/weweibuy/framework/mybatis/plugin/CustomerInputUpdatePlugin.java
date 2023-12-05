b44ed70df3ca7750fca25cdca17fec7733c07a01
durenhao <durenhao@g7.com.cn>
2023-12-05 14:03:36 +0800

    sql 生成插件

--- mybatis-plugin/src/main/java/com/weweibuy/framework/mybatis/plugin/CustomerInputUpdatePlugin.java
+++ mybatis-plugin/src/main/java/com/weweibuy/framework/mybatis/plugin/CustomerInputUpdatePlugin.java
@@ -20,17 +20,37 @@ import java.util.List;
  * @author durenhao
  * @date 2023/2/25 20:51
  **/
-public class CustomerUpdatePlugin extends BasePlugin {
+public class CustomerInputUpdatePlugin extends BasePlugin {
 
     @Override
     public boolean sqlMapUpdateByExampleSelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
-        List<Element> elements = element.getElements();
-        List<Element> elementsList = ((XmlElement) elements.get(1)).getElements();
-        XmlElement ifUpdateSqlElement = new XmlElement("if");
-        ifUpdateSqlElement.addAttribute(new Attribute("test", "example.updateSql != null"));
-        ifUpdateSqlElement.addElement(new TextElement("${example.updateSql} ,"));
-        elementsList.add(ifUpdateSqlElement);
-        return super.sqlMapSelectByExampleWithBLOBsElementGenerated(element, introspectedTable);
+        addAndRightPosition(element.getElements());
+        return super.sqlMapUpdateByExampleSelectiveElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapUpdateByPrimaryKeySelectiveElementGenerated(
+            XmlElement element, IntrospectedTable introspectedTable) {
+        addAndRightPosition(element.getElements());
+        return super.sqlMapUpdateByPrimaryKeySelectiveElementGenerated(element, introspectedTable);
+    }
+
+
+    private XmlElement buildGroupByElement() {
+        XmlElement updateSqlElement = new XmlElement("if");
+        updateSqlElement.addAttribute(new Attribute("test", "example.updateSql != null"));
+        updateSqlElement.addElement(new TextElement("${example.updateSql},"));
+        return updateSqlElement;
+    }
+
+
+    private void addAndRightPosition(List<Element> topElements) {
+        topElements.stream()
+                .filter(e -> e instanceof XmlElement && "set".equals(((XmlElement) e).getName()))
+                .map(e -> ((XmlElement) e).getElements())
+                .filter(e -> e != null && !e.isEmpty())
+                .findFirst()
+                .ifPresent(e -> e.add(buildGroupByElement()));
     }
 
     @Override
@@ -66,6 +86,7 @@ public class CustomerUpdatePlugin extends BasePlugin {
                 topLevelClass.getType(),
                 new Parameter(FullyQualifiedJavaType.getStringInstance(), "updateSqlClauses", true)
         );
+
         commentGenerator.addGeneralMethodComment(orderByMethod1, introspectedTable);
         orderByMethod1 = JavaElementGeneratorTools.generateMethodBody(
                 orderByMethod1,
